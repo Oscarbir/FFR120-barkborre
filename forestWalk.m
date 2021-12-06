3b4cc742ca5090ee927ad289b0bf20eefe9253b8
Wille <williamtruve@gmail.com>
2021-12-06 14:24:15 +0100

    håll tummarna

--- forestWalk.m
+++ forestWalk.m
@@ -1,6 +1,7 @@
 function forest = forestWalk(forest,invFreq,i)
 if mod(i,invFreq)==0
-        if rand < length(find(forest==2)*0.05)
+    r = rand;
+        if r < length(find(forest==2))*0.01
             [rows,cols]= find(forest==2);
             forest(rows,cols) = 0;
             [rows1,cols1] = find(forest==1);
--- spreading.m
+++ spreading.m
@@ -5,38 +5,38 @@ function  [forest, nrOfTrees] = spreading(forest,N,tree,nrOfTrees,meanTemp)
 if meanTemp>=13 && forest(row,column)==1
     spreadProb=meanTemp*0.02; 
     if rand < spreadProb           %Ändra till korrekt sannolikhet senare
-       forest(row,column)=3;
+       forest(row,column)=2;
     end
-      if length(find(forest==3))>0        
-           [row,col]=find(forest==3);
+      if length(find(forest==2))>0        
+           [row,col]=find(forest==2);
 
            if forest(min(row(1)+1,N),col(1))==1 && rand < spreadProb %Fixa rätt sannolikhet  
-               forest(min(row(1)+1,N),col(1))=3;
+               forest(min(row(1)+1,N),col(1))=2;
            end
            if forest(max(row(1)-1,1),col(1))==1 && rand < spreadProb
-               forest(max(row(1)-1,1),col(1))=3;
+               forest(max(row(1)-1,1),col(1))=2;
            end
            if forest(row(1),min(col(1)+1,N))==1 && rand < spreadProb
-               forest(row(1),min(col(1)+1,N))=3;
+               forest(row(1),min(col(1)+1,N))=2;
            end
            if forest(row(1),max(col(1)-1,1))==1 && rand < spreadProb
-               forest(row(1),max(col(1)-1,1))=3;
+               forest(row(1),max(col(1)-1,1))=2;
            end
            
            if forest(max(row(1)-1,1),max(col(1)-1,1))==1 && rand < spreadProb
-               forest(max(row(1)-1,1),max(col(1)-1,1))=3; % Vänsterupp
+               forest(max(row(1)-1,1),max(col(1)-1,1))=2; % Vänsterupp
            end
            
            if forest(min(row(1)+1,N),max(col(1)-1,1))==1 && rand < spreadProb
-               forest(min(row(1)+1,N),max(col(1)-1,1))=3;   %VänsterNer
+               forest(min(row(1)+1,N),max(col(1)-1,1))=2;   %VänsterNer
            end
            
            if forest(max(row(1)-1,1),min(col(1)+1,N))==1 && rand < spreadProb
-               forest(max(row(1)-1,1),min(col(1)+1,N))=3;   %Högerupp
+               forest(max(row(1)-1,1),min(col(1)+1,N))=2;   %Högerupp
            end
            
            if forest(min(row(1)+1,N),min(col(1)+1,N))==1 && rand < spreadProb
-               forest(min(row(1)+1,N),min(row(1)+1,N))= 3;   %Högerner
+               forest(min(row(1)+1,N),min(row(1)+1,N))= 2;   %Högerner
            end
            
            forest(row(1),col(1))=2;
