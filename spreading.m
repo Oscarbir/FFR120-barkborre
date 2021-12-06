7eeca090576b091d6f01fad001e63719456a5c1a
Oscar Birging <oscarbi@student.chalmers.se>
2021-12-06 14:49:29 +0100

    spread

--- spreading.m
+++ spreading.m
@@ -5,41 +5,40 @@ function  [forest, nrOfTrees] = spreading(forest,N,tree,nrOfTrees,meanTemp)
 if meanTemp>=13 && forest(row,column)==1 
     spreadProb=meanTemp*0.02;  
     if rand < spreadProb           %Ändra till korrekt sannolikhet senare 
-       forest(row,column)=3; 
+       forest(row,column)=2; 
     end 
-      if length(find(forest==3))>0         
-           [row,col]=find(forest==3); 
+      if length(find(forest==2))>0         
+           [row,col]=find(forest==2); 
          for n=1:length(row) 
            if forest(min(row(n)+1,N),col(n))==1 && rand < spreadProb %Fixa rätt sannolikhet   
-               forest(min(row(1)+1,N),col(n))=3; 
+               forest(min(row(1)+1,N),col(n))=2; 
            end 
            if forest(max(row(n)-1,1),col(n))==1 && rand < spreadProb 
-               forest(max(row(n)-1,1),col(n))=3; 
+               forest(max(row(n)-1,1),col(n))=2; 
            end 
            if forest(row(n),min(col(n)+1,N))==1 && rand < spreadProb 
-               forest(row(n),min(col(n)+1,N))=3; 
+               forest(row(n),min(col(n)+1,N))=2; 
            end 
            if forest(row(n),max(col(n)-1,1))==1 && rand < spreadProb 
-               forest(row(n),max(col(n)-1,1))=3; 
+               forest(row(n),max(col(n)-1,1))=2; 
            end 
             
            if forest(max(row(n)-1,1),max(col(n)-1,1))==1 && rand < spreadProb 
-               forest(max(row(n)-1,1),max(col(n)-1,1))=3; % Vänsterupp 
+               forest(max(row(n)-1,1),max(col(n)-1,1))=2; % Vänsterupp 
            end 
             
            if forest(min(row(n)+1,N),max(col(n)-1,1))==1 && rand < spreadProb 
-               forest(min(row(n)+1,N),max(col(n)-1,1))=3;   %VänsterNer 
+               forest(min(row(n)+1,N),max(col(n)-1,1))=2;   %VänsterNer 
            end 
             
            if forest(max(row(n)-1,1),min(col(n)+1,N))==1 && rand < spreadProb 
-               forest(max(row(n)-1,1),min(col(n)+1,N))=3;   %Högerupp 
+               forest(max(row(n)-1,1),min(col(n)+1,N))=2;   %Högerupp 
            end 
             
            if forest(min(row(n)+1,N),min(col(n)+1,N))==1 && rand < spreadProb 
-               forest(min(row(n)+1,N),min(row(n)+1,N))= 3;   %Högerner 
+               forest(min(row(n)+1,N),min(row(n)+1,N))=2;   %Högerner 
            end 
             
-           forest(row(n),col(n))=2; 
            tree=tree+1; 
  
       nrOfTrees=[nrOfTrees tree]; 
