2b20a8225571ca8da4d9fcaad91cbe89c1426fd5
filipsterner <94842494+filipsterner@users.noreply.github.com>
2021-12-06 14:22:18 +0100

    Add files via upload

--- plot_forest.m
+++ plot_forest.m
@@ -1,19 +1,20 @@
 function plot_forest(forest,meanTemp,t)
+    hold on
+    pl=subplot(11,1,9:11);
+    pl.Position = [0.395 0.1 0.25 0.2];
     
-    subplot(11,1,1:7)
-    hold off
+    hold on
+    max(1,t-40)
+    plot(max(1,t-40):t,meanTemp(max(1,t-40):t),'g-')
+    axis([t-40 t -30 30])
+    
+    subplot(11,1,1:7);
+    hold on
     [Rows1,Cols1] = find(forest==1);
     [Rows2,Cols2] = find(forest==2);
     scatter(Rows1,Cols1,'g','.');
-    hold on
     scatter(Rows2,Cols2,'k','.');
     axis square
-    pl=subplot(11,1,9:11);
-    pl.Position = [0.395 0.1 0.25 0.2];
-    if t>1
-        hold on
-        plot([t-1 t],[meanTemp(t-1) meanTemp(t)],'g-')
-        axis([t-40 t -30 30])
-        pause(0.3)
-    end
+    pause(0.3)
+    
 end
