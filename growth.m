b44fec7c1fac8fab8c9d5f7a35cdb809750726c0
Wille <williamtruve@gmail.com>
2021-12-06 14:34:45 +0100

    slarvfel

--- growth.m
+++ growth.m
@@ -24,15 +24,15 @@ if meanTemp>5 && meanTemp<18 %För att plantera träd får det inte vara för ka
                forest(max(x(randx)-1,11),max(y(randy)-1,1))=1; % Vänsterupp
            end
            
-           if forest(min(x(randx)+1,N),max(y(randy)-1,1))==0 && rand < spreadProb
+           if forest(min(x(randx)+1,N),max(y(randy)-1,1))==0 
                forest(min(x(randx)+1,N),max(y(randy)-1,1))=1;   %VänsterNer
            end
            
-           if forest(max(x(randx)-1,1),min(y(randy)+1,N))==0 && rand < spreadProb
+           if forest(max(x(randx)-1,1),min(y(randy)+1,N))==0 
                forest(max(x(randx)-1,1),min(y(randy)+1,N))=1;   %Högerupp
            end
            
-           if forest(min(x(randx)+1,N),min(y(randy)+1,N))==0 && rand < spreadProb
+           if forest(min(x(randx)+1,N),min(y(randy)+1,N))==0
                forest(min(x(randx)+1,N),min(y(randy)+1,N))= 1;   %Högerner
            end
            
